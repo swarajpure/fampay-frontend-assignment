dda1611625fe1e198fd457b6a1205f3f23e2115a
Swaraj <swarajrajpure@gmail.com>
2021-05-19 01:05:10 +0530

    feat: dynamic-width-cards

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import './App.css';
 import ShowBigDisplayCards from './components/BigDisplayCards/ShowBigDisplayCards';
 import ShowSmallCardsArrow from './components/SmallCardsArrow/ShowSmallCardsArrow';
 import ShowImageCards from './components/ImageCards/ShowImageCards';
+import ShowDynamicWidthCards from './components/DynamicWidthCards/ShowDynamicWidthCards';
 import filterFetchedData from './utils/filterFetchedData';
 
 const App = () => {
   const [bigDisplayCards, setBigDisplayCards] = useState();
   const [smallCardsArrow, setSmallCardsArrow] = useState();
   const [imageCards, setImageCards] = useState();
+  const [dynamicWidthCards, setDynamicWidthCards] = useState();
 
   useEffect(() => {
     filterFetchedData().then((data) => {
       setBigDisplayCards(data.bigDisplayCards);
       setSmallCardsArrow(data.smallCardsArrow);
       setImageCards(data.imgCards);
+      setDynamicWidthCards(data.dynamicWidthCards);
     });
   }, []);
 
@@ -23,6 +26,7 @@ const App = () => {
       <ShowBigDisplayCards cardsData={bigDisplayCards} />
       <ShowSmallCardsArrow cardsData={smallCardsArrow} />
       <ShowImageCards cardsData={imageCards} />
+      <ShowDynamicWidthCards cardsData={dynamicWidthCards} />
     </div>
   );
 };

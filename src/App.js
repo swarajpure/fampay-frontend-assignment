38a2eee3530ce7acefe0c4bbfa8f1c6d71b78307
Swaraj <swarajrajpure@gmail.com>
2021-05-19 10:25:40 +0530

    feat: pull down to refresh

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import PullToRefresh from 'react-simple-pull-to-refresh';
 import ShowBigDisplayCards from './components/BigDisplayCards/ShowBigDisplayCards';
 import ShowSmallCardsArrow from './components/SmallCardsArrow/ShowSmallCardsArrow';
 import ShowImageCards from './components/ImageCards/ShowImageCards';
@@ -13,6 +14,7 @@ const App = () => {
   const [imageCards, setImageCards] = useState();
   const [dynamicWidthCards, setDynamicWidthCards] = useState();
   const [smallCardsNoArrow, setSmallCardsNoArrow] = useState();
+  const [boolToRefresh, setBoolToRefresh] = useState(false);
 
   useEffect(() => {
     filterFetchedData().then((data) => {
@@ -22,16 +24,24 @@ const App = () => {
       setDynamicWidthCards(data.dynamicWidthCards);
       setSmallCardsNoArrow(data.smallCardsNoArrow);
     });
-  }, []);
+  }, [boolToRefresh]);
+
+  const refreshHandler = () => {
+    const currentValue = boolToRefresh;
+    setBoolToRefresh(!currentValue);
+  };
 
   return (
     <div className="main-container">
-      <ShowBigDisplayCards cardsData={bigDisplayCards} />
-      <ShowSmallCardsArrow cardsData={smallCardsArrow} />
-      <ShowImageCards cardsData={imageCards} />
-      <ShowDynamicWidthCards cardsData={dynamicWidthCards} />
-      <ShowSmallCardsNoArrow cardsData={smallCardsNoArrow} />
+      <PullToRefresh onRefresh={refreshHandler}>
+        <ShowBigDisplayCards cardsData={bigDisplayCards} />
+        <ShowSmallCardsArrow cardsData={smallCardsArrow} />
+        <ShowImageCards cardsData={imageCards} />
+        <ShowDynamicWidthCards cardsData={dynamicWidthCards} />
+        <ShowSmallCardsNoArrow cardsData={smallCardsNoArrow} />
+      </PullToRefresh>
     </div>
+
   );
 };
 
